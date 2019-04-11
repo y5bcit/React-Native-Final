b2ed3da79724e53f0c7a6e31d101d6e052150646
y5bcit <yruan5@my.bcit.ca>
2019-04-11 16:00:13 -0700

    update Expo and packages.

--- App.js
+++ App.js
@@ -1,30 +1,21 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-import { createStackNavigator, createAppContainer } from "react-navigation";
 
-import HomeScreen from './components/hompage'
-import MapComponent from './components/MapComponent'
-import DummyComponent from './components/DummyComponent'
-
-const RootStack = createStackNavigator({
-    Home: {
-      screen: HomeScreen
-    },
-    Maps: {
-      screen: MapComponent
-    },
-    Gallery: {
-      screen: DummyComponent
-    },
-    Animation: {
-      screen: DummyComponent
-    }
-  },
-  {
-    initialRouteName: "Home"
+export default class App extends React.Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text>Open up App.js to start working on your app!</Text>
+      </View>
+    );
   }
-);
+}
 
-const App = createAppContainer(RootStack);
-
-export default App;
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
