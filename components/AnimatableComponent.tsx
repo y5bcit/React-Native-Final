475ca391ef0292720aabd02c122ef65a55aafa8b
y5bcit <yruan5@my.bcit.ca>
2019-04-11 19:30:35 -0700

    add more animatable

--- components/AnimatableComponent.tsx
+++ components/AnimatableComponent.tsx
@@ -1,6 +1,9 @@
 import React from "react";
-import { Text, View, Button } from "react-native";
+import { Dimensions, Text, View, Button } from "react-native";
 import * as Animatable from "react-native-animatable"
+
+const screenHeight = Dimensions.get("window").height
+const screenWidth = Dimensions.get("window").width
 
 export default class AnimatableComponent extends React.Component<{}, {}> {
     constructor(props) {
@@ -11,13 +14,27 @@ export default class AnimatableComponent extends React.Component<{}, {}> {
 
     render() {
         return (
-            <Animatable.View>
-                <Button title={"ButtonTitleText"} onPress={(ev) => {
-                    console.log("Pressed.");
-                    this.text.transition({ top: 0 }, { top: 200 }, 5000, "linear");
-                }}></Button>
-                <Animatable.Text ref={this.handleTextRef}>Moving down when clicked</Animatable.Text>
-            </Animatable.View>
+            <View>
+                <View>
+                    <Button title={"ButtonTitleText"} onPress={(ev) => {
+                        console.log("Pressed.");
+                        this.text.transition({ top: 0 }, { top: 200 }, 5000, "linear");
+                    }}></Button>
+                    <Animatable.Text ref={this.handleTextRef}>Moving down when clicked</Animatable.Text>
+                </View>
+                <View>
+                    <Animatable.Text animation={{
+                        from: { rotate: '0deg' },
+                        to: { rotate: '360deg' },
+                    }} duration={10000} iterationCount={"infinite"}>Infinite rotation</Animatable.Text>
+                </View>
+                <View>
+                    <Animatable.Text animation={{
+                        from: { translateY: 0 },
+                        to: { translateY: screenHeight * 0.8 },
+                    }} duration={10000} easing={"ease-in-cubic"}>Accelerated falling</Animatable.Text>
+                </View>
+            </View>
         )
     }
 }
---
+++ components/AnimatedComponent.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { Text, View, Button } from "react-native";
+
+export default class AnimatedComponent extends React.Component<{}, {}> {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        // Not implemented yet
+        return (
+            <View>
+            </View>
+        )
+    }
+}
